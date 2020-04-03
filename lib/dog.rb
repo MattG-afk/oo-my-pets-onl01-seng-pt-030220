d4d454c9a6458f9a8f7024afebf19bc2a9b10b19
Matt Gordon <bigmattsize21@gmail.com>
2020-04-03 23:25:10 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,10 +1,16 @@
 class Dog
   attr_reader :name
-  attr_accessor :mood
+  attr_accessor :mood, :owner
+  
+  @@all= []
 
-  def initialize(name, mood= "nervous")
+  def initialize(name, mood= "nervous", owner)
     @mood = mood
     @name = name
+    @@all << self
   end
   
+  def self.all
+    @@all
+  end
 end
