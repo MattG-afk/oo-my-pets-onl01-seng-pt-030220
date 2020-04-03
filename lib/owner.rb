daa52168201cd5221b552d4243382d62d28c04df
Matt Gordon <bigmattsize21@gmail.com>
2020-04-03 23:43:32 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -74,7 +74,7 @@ class Owner
   end
   
   def list_pets
-    "I have #{pets[:dogs].length} dog(s), and #{pets[:cats].length} cat(s)."
+    "I have #{pets.dogs.length} dog(s), and #{pets.cats.length} cat(s)."
   end
   
   def save 
